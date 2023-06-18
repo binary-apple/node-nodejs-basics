ff97ae0377f22c80faeca67307d496b0df5c4172
Anna Perevedentseva <anna.pereved@gmail.com>
2023-06-18 23:26:19 +0200

    feat: implement delete-subtask

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import { access, rm } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+    const filePath = path.resolve(__dirname, 'files', 'fileToRemove.txt');
+    access(filePath, (err) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        }
+        rm(filePath, () => {});
+    });
 };
 
 await remove();

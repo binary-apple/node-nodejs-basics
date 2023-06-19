f79f3dca3feeec326c4c89fb3d5c37e1d4a28da7
Anna Perevedentseva <anna.pereved@gmail.com>
2023-06-19 17:41:10 +0200

    feat: implement streams/read-subtask

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import { createReadStream } from 'fs';
+const { stdout } = process;
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const inStream = createReadStream(path.resolve(__dirname,'files', 'fileToRead.txt'), 'utf-8');
+    inStream.on('error', err => console.log('Error', err.message));
+    inStream.pipe(stdout);
 };
 
 await read();

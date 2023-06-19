dd84712fa7af1c09c3996e6f753e6fe2fbceccb6
Anna Perevedentseva <anna.pereved@gmail.com>
2023-06-19 14:31:29 +0200

    feat: implement copy-subtask

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,22 @@
+import { readdir, copyFile, mkdir } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+    const dirFrom = path.resolve(__dirname, 'files');
+    const dirTo = path.resolve(__dirname, 'files_copy');
+    try {
+        await mkdir(dirTo);
+        const filesToCopy = await readdir(dirFrom, { withFileTypes: true});
+        for (const file of filesToCopy) {
+            await copyFile(path.resolve(dirFrom, file.name), path.resolve(dirTo, file.name));
+        }
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await copy();

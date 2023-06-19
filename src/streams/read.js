a2d9b6c4653cd7a348c24778c901a3e474275b4b
Anna Perevedentseva <anna.pereved@gmail.com>
2023-06-19 18:15:00 +0200

    feat: implement streams/write-subtask

--- src/streams/read.js
+++ src/streams/read.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const read = async () => {
-    const inStream = createReadStream(path.resolve(__dirname,'files', 'fileToRead.txt'), 'utf-8');
+    const inStream = createReadStream(path.resolve(__dirname, 'files', 'fileToRead.txt'), 'utf-8');
     inStream.on('error', err => console.log('Error', err.message));
     inStream.pipe(stdout);
 };
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,22 @@
+import { createWriteStream } from 'fs';
+const { stdin } = process;
+import * as readline from 'readline/promises';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    const outStream = createWriteStream(path.resolve(__dirname, 'files', 'fileToWrite.txt'));
+    const rl = readline.createInterface({ input: stdin, output: outStream });
+
+    rl.on('line', (input) => {
+        outStream.write(input + '\n');
+    });
+
+    rl.on('SIGINT', rl.close);
 };
 
 await write();

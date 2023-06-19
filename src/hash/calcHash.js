19ffdf88373c3401350bb7702f20ac42ad774d41
Anna Perevedentseva <anna.pereved@gmail.com>
2023-06-19 15:31:16 +0200

    feat: implement hash-subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const file = await readFile(path.resolve(__dirname, 'files', 'fileToCalculateHashFor.txt'), { encoding: 'utf8' });
+    const hash = createHash('sha256').update(file).digest('hex');
+    console.log(hash);
 };
 
 await calculateHash();

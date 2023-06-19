d49d36438fa3f11534d52cf542bfff9af3451cf0
Anna Perevedentseva <anna.pereved@gmail.com>
2023-06-19 11:36:55 +0200

    feat: implement list-subtask

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import { readdir, copyFile } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+    try {
+        const files = await readdir(path.resolve(__dirname, 'files'));
+        for (const file of files) {
+            console.log(file);
+        }
+    } catch (err) {
+        throw new Error('FS operation failed');
+    } 
 };
 
 await list();

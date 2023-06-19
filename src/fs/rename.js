99bfaaa9814294f5101b2c4beebd4c225009e8d3
Anna Perevedentseva <anna.pereved@gmail.com>
2023-06-19 12:55:36 +0200

    refactor: reorganize try-catch blocks

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -5,34 +5,20 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-class CustomError extends Error {
-    constructor(message) {
-        super(message);
-        this.name = this.constructor.name;
-    }
-}
-
 const rename = async () => {
+    const oldPath = path.resolve(__dirname, 'files', 'wrongFilename.txt');
+    const newPath = path.resolve(__dirname, 'files', 'properFilename.md');        
     try {
-        const oldPath = path.resolve(__dirname, 'files', 'wrongFilename.txt');
-        const newPath = path.resolve(__dirname, 'files', 'properFilename.md');        
+        await stat(newPath);
+    } catch(err) {
         try {
-            await stat(newPath);
-            throw new CustomError('properFilename.md is already exists');
-        } catch(err) {
-            if (err instanceof CustomError) {
-                throw err;
-            } else {
-                try {
-                    await nodeRename(oldPath, newPath);
-                } catch(err) {
-                    throw err;
-                }
-            }
-        }   
-    } catch (err) {
-        throw new Error('FS operation failed');
+            await nodeRename(oldPath, newPath);
+            return;
+        } catch {
+            throw new Error('FS operation failed');
+        }
     }
+    throw new Error('FS operation failed');
 };
 
 await rename();

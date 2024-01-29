b5da680c01b6fa3c56b95468b2918b38dda78fd6
Anna Perevedentseva <anna.pereved@gmail.com>
2024-01-29 17:19:55 +0100

    feat: implement wt/worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,16 @@
+import { Worker } from 'worker_threads';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const workerPath = path.resolve(__dirname, 'worker.js');
+
 const performCalculations = async () => {
-    // Write your code here
+    const worker = new Worker(workerPath, {workerData: {n: 6}});
+    worker.on('message', (msg) => { console.log(msg); });
 };
 
 await performCalculations();

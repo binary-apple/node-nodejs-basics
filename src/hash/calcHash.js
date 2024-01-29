5cdba430273d294bd5d03874335f22a3c3c62fb1
Anna Perevedentseva <anna.pereved@gmail.com>
2024-01-29 20:14:37 +0100

    fix: implement hash-subtask using streams API

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,15 +1,16 @@
 import { createHash } from 'crypto';
-import { readFile } from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { createReadStream } from 'fs';
+import { stdout } from 'process';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const calculateHash = async () => {
-    const file = await readFile(path.resolve(__dirname, 'files', 'fileToCalculateHashFor.txt'), { encoding: 'utf8' });
-    const hash = createHash('sha256').update(file).digest('hex');
-    console.log(hash);
+    const hash = createHash('sha256');
+    const input = createReadStream(path.resolve(__dirname, 'files', 'fileToCalculateHashFor.txt'));
+    input.pipe(hash).setEncoding('hex').pipe(stdout);
 };
 
 await calculateHash();
